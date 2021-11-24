9b198821af91696d0cddbeac4a23d36d3c61372b
syedamariumhasnain <syedamarium27@gmail.com>
2021-11-24 17:16:49 +0500

    added custom form-hook, added UpdatePlace

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -109,9 +109,9 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: "",
+    value: props.initialValue || "",
     isTouched: false,
-    isValid: false,
+    isValid: props.initialValid || false,
   });
 
   // // Here dependences [props, inputState] can cause infinite looping
