ece03fdde8931329e4add7e3869c748d664e3cfa
syedamariumhasnain <syedamarium27@gmail.com>
2022-01-03 11:11:11 +0500

    shift auth code to useAuth hook + little changes in newPlace page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,69 +13,10 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
-
-// We are setting this outside of our app component cuz we it's some behind
-// the scene data we need to manage, not the data which should re-render as
-// component (not state of component, just general variable)
-let logoutTimer;
+import { useAuth } from "./shared/hooks/auth-hook";
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [tokenExpiration, setTokenExpiration] = useState();
-  const [userId, setUserId] = useState();
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpiration(tokenExpirationDate);
-    // we use JSON.stringify({}) because to localStorage we can only
-    // entre text and we have to set object
-    // Storing date in ISOString format will protect all the date
-    // information and can be easily converted back to date object later
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpiration(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpiration) {
-      const remainingTime = tokenExpiration.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpiration]);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(
-        storedData.uid,
-        storedData.token,
-        new Date(storedData.expiration)
-      );
-    }
-    // we can also add a state for representing the status of this localStorage check
-    // So, we can add loading screen at that time
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
